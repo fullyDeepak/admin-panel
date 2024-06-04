0d97ee2adcd8447fdbbcc1ed4e60448488df18ba
Deepak Shreevastava <dpkshreevastava@gmail.com>
2024-06-04 16:26:46 +0530

    chore: transition duration

--- src/components/ui/ProgressBar.tsx
+++ src/components/ui/ProgressBar.tsx
@@ -7,7 +7,7 @@ export default function ProgressBar({ progress = 50 }: ProgressBarProps) {
     <div>
       <div
         style={{ marginInlineStart: `calc(${progress}% - 1.25rem)` }}
-        className={`mb-2 inline-block rounded-lg border ${progress >= 95 ? 'border-green-200' : 'border-blue-200'} bg-blue-50 px-1.5 py-0.5 text-xs font-medium ${progress >= 95 ? 'text-green-600' : 'text-blue-600'} transition-all duration-100`}
+        className={`mb-2 inline-block rounded-lg border ${progress >= 95 ? 'border-green-200' : 'border-blue-200'} bg-blue-50 px-1.5 py-0.5 text-xs font-medium ${progress >= 95 ? 'text-green-600' : 'text-blue-600'} transition-all duration-200`}
       >
         {progress}%
       </div>
@@ -20,7 +20,7 @@ export default function ProgressBar({ progress = 50 }: ProgressBarProps) {
       >
         <div
           style={{ width: `${progress}%` }}
-          className={`flex flex-col justify-center overflow-hidden whitespace-nowrap rounded-full ${progress >= 95 ? 'bg-green-600' : 'bg-blue-600'} text-center text-xs text-white transition-all duration-100`}
+          className={`flex flex-col justify-center overflow-hidden whitespace-nowrap rounded-full ${progress >= 95 ? 'bg-green-600' : 'bg-blue-600'} text-center text-xs text-white transition-all duration-200`}
         ></div>
       </div>
     </div>
