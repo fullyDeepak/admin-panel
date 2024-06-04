491fda3580a92681132a14a65c1687ab6b0490f0
Deepak Shreevastava <dpkshreevastava@gmail.com>
2024-06-04 15:44:56 +0530

    feat: new component progress bar

---
+++ src/components/ui/ProgressBar.tsx
@@ -0,0 +1,28 @@
+type ProgressBarProps = {
+  progress: number;
+};
+
+export default function ProgressBar({ progress = 50 }: ProgressBarProps) {
+  return (
+    <div>
+      <div
+        style={{ marginInlineStart: `calc(${progress}% - 1.25rem)` }}
+        className={`mb-2 inline-block rounded-lg border ${progress >= 95 ? 'border-green-200' : 'border-blue-200'} bg-blue-50 px-1.5 py-0.5 text-xs font-medium ${progress >= 95 ? 'text-green-600' : 'text-blue-600'}`}
+      >
+        {progress}%
+      </div>
+      <div
+        className='flex h-2 w-full overflow-hidden rounded-full bg-gray-200'
+        role='progressbar'
+        aria-valuenow={progress}
+        aria-valuemin={0}
+        aria-valuemax={100}
+      >
+        <div
+          style={{ width: `${progress}%` }}
+          className={`flex flex-col justify-center overflow-hidden whitespace-nowrap rounded-full ${progress >= 95 ? 'bg-green-600' : 'bg-blue-600'} text-center text-xs text-white transition duration-500`}
+        ></div>
+      </div>
+    </div>
+  );
+}
