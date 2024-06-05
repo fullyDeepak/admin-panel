9ea2038dbd839de7ca8ece96566d59d3e1d21bf5
Deepak Shreevastava <dpkshreevastava@gmail.com>
2024-06-05 14:09:28 +0530

    chore: seprated component for form

---
+++ src/app/forms/image-tagging/Form.tsx
@@ -0,0 +1,164 @@
+import LoadingCircle from '@/components/ui/LoadingCircle';
+import { useImageFormStore } from '@/store/useImageFormStore';
+import { Dispatch, FormEventHandler, SetStateAction, useId } from 'react';
+import Select from 'react-select';
+import UnitFP from './UnitFP';
+import TowerFP from './TowerFP';
+import ProgressBar from '@/components/ui/ProgressBar';
+
+type FormProps = {
+  projectOptions:
+    | {
+        value: number;
+        label: string;
+      }[]
+    | undefined;
+  submitForm: FormEventHandler<HTMLFormElement> | undefined;
+  loadingProjectOptions: boolean;
+  progress: number;
+  setProjectFiles: Dispatch<SetStateAction<FileList | null>>;
+  setResultData: (
+    newData:
+      | {
+          fileName: string;
+          uploadStatus: 'Success' | 'Failure';
+        }[]
+      | null
+  ) => void;
+};
+
+export default function Form({
+  loadingProjectOptions,
+  projectOptions,
+  submitForm,
+  progress,
+  setProjectFiles,
+  setResultData,
+}: FormProps) {
+  const {
+    selectedProject,
+    setSelectedProject,
+    selectedImageTaggingType,
+    setSelectedImageTaggingType,
+    loadingTowerFloorData,
+    towerFloorFormData,
+    uploadingStatus,
+    setUploadingStatus,
+  } = useImageFormStore();
+  return (
+    <form
+      className='mt-5 flex w-full max-w-full flex-col gap-4 self-center rounded p-10 text-sm shadow-none md:max-w-[60%] md:text-lg md:shadow-[0_3px_10px_rgb(0,0,0,0.2)]'
+      id='projectTowerImageTagging'
+      onSubmit={submitForm}
+    >
+      <label className='flex flex-wrap items-center justify-between gap-5'>
+        <span className='flex-[3] text-base md:text-xl'>Select Project:</span>
+        <Select
+          className='w-full flex-[5]'
+          key={'projectOptions'}
+          isClearable
+          instanceId={useId()}
+          value={selectedProject}
+          onChange={(e) => {
+            setSelectedProject(e);
+            setResultData(null);
+          }}
+          options={projectOptions}
+          isLoading={loadingProjectOptions}
+        />
+      </label>
+      <label className='flex flex-wrap items-center justify-between gap-5'>
+        <span className='flex-[3] text-base md:text-xl'>Tagging Type:</span>
+        <Select
+          className='w-full flex-[5]'
+          key={'district'}
+          isClearable
+          isSearchable={false}
+          instanceId={useId()}
+          value={selectedImageTaggingType}
+          onChange={(e) => {
+            setSelectedImageTaggingType(e);
+            setResultData(null);
+          }}
+          options={[
+            { label: 'Brochure', value: 'brochure' },
+            { label: 'Project Master Plan', value: 'project_master_plan' },
+            { label: 'Project Images', value: 'project_image' },
+            { label: 'Tower Floor Plan', value: 'tower-fp' },
+            { label: 'Unit Floor Plan', value: 'unit-fp' },
+          ]}
+        />
+      </label>
+      {loadingTowerFloorData === 'loading' && (
+        <span className='text-center'>
+          <LoadingCircle circleColor='black' size='large' />
+        </span>
+      )}
+      {loadingTowerFloorData === 'complete' &&
+        selectedImageTaggingType?.value === 'unit-fp' &&
+        towerFloorFormData &&
+        towerFloorFormData?.length > 0 && (
+          <UnitFP towerFloorData={towerFloorFormData} />
+        )}
+      {loadingTowerFloorData === 'complete' &&
+        towerFloorFormData &&
+        towerFloorFormData.length === 0 &&
+        selectedProject?.value &&
+        selectedImageTaggingType?.value === 'unit-fp' && (
+          <span className='text-center font-semibold text-error'>
+            Units not available.
+          </span>
+        )}
+      {loadingTowerFloorData === 'error' && (
+        <span className='text-center font-semibold text-error'>
+          Something went wrong
+        </span>
+      )}
+      {loadingTowerFloorData === 'complete' &&
+        selectedImageTaggingType?.value === 'tower-fp' &&
+        towerFloorFormData &&
+        towerFloorFormData?.length > 0 && (
+          <TowerFP towerFloorData={towerFloorFormData} />
+        )}
+      {selectedImageTaggingType &&
+        selectedImageTaggingType?.value !== 'unit-fp' &&
+        selectedImageTaggingType?.value !== 'tower-fp' && (
+          <label className='relative flex flex-wrap items-center justify-between gap-5'>
+            <span className='flex-[3] text-base md:text-xl'>Select File:</span>
+            <input
+              type='file'
+              className='file-input file-input-bordered ml-2 h-10 flex-[5]'
+              multiple
+              id='project-input-file'
+              accept='image/*,.pdf'
+              onChange={(e) => {
+                setResultData(null);
+                setUploadingStatus('idle');
+                setProjectFiles(e.target.files);
+              }}
+            />
+          </label>
+        )}
+      {selectedImageTaggingType && selectedImageTaggingType?.value && (
+        <>
+          <button
+            className='btn-rezy btn mx-auto min-w-40 disabled:text-gray-600'
+            disabled={uploadingStatus === 'running' ? true : false}
+          >
+            {uploadingStatus === 'idle' || uploadingStatus === 'complete'
+              ? 'Submit'
+              : 'Uploading...'}
+          </button>
+          {uploadingStatus === 'idle' ? (
+            <></>
+          ) : uploadingStatus === 'running' ||
+            uploadingStatus === 'complete' ? (
+            <ProgressBar progress={progress} />
+          ) : (
+            <></>
+          )}
+        </>
+      )}
+    </form>
+  );
+}
