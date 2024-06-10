8f079f6b5c5f9691544f19edb34b58d0d257fc21
Deepak Shreevastava <dpkshreevastava@gmail.com>
2024-06-10 12:46:33 +0530

    chore: new entry

--- src/app/forms/page.tsx
+++ src/app/forms/page.tsx
@@ -14,36 +14,38 @@ export default function FormHomePage() {
           </h2>
           <Link
             href={'/forms/village-replacement'}
-            className=' btn flex items-center active:btn-success hover:text-black active:text-white'
+            className='btn flex items-center active:btn-success hover:text-black active:text-white'
           >
             Village Replacement
           </Link>
           <Link
             href={'/forms/village-onboarding'}
-            className='btn  flex items-center active:btn-success hover:text-black active:text-white'
+            className='btn flex items-center active:btn-success hover:text-black active:text-white'
           >
             Village Onboarding
           </Link>
           <Link
             href={'/forms/sro-scraper'}
-            className='btn  flex items-center active:btn-success hover:text-black active:text-white'
+            className='btn flex items-center active:btn-success hover:text-black active:text-white'
           >
             SRO Scraper
           </Link>
         </div>
         <div className='flex w-fit flex-col gap-4 text-gray-500'>
-          <h2 className='text-center text-xl font-semibold'>RERA</h2>
+          <h2 className='text-center text-xl font-semibold'>
+            Correction Forms
+          </h2>
           <Link
             href={'/forms/rera-correction'}
-            className='btn  flex items-center active:btn-success hover:text-black active:text-white'
+            className='btn flex items-center active:btn-success hover:text-black active:text-white'
           >
-            DMV Correction Form
+            RERA DMV
           </Link>
           <Link
-            href={'/forms/rera-project-status'}
-            className='btn  flex items-center active:btn-success hover:text-black active:text-white'
+            href={'/forms/unit-master-correction'}
+            className='btn flex items-center active:btn-success hover:text-black active:text-white'
           >
-            Project Status
+            UM Manual
           </Link>
         </div>
         <div className='flex w-fit flex-col gap-4 text-gray-500'>
@@ -52,19 +54,19 @@ export default function FormHomePage() {
           </h2>
           <Link
             href={'/forms/project-tower-rera'}
-            className='btn  flex items-center active:btn-success hover:text-black active:text-white'
+            className='btn flex items-center active:btn-success hover:text-black active:text-white'
           >
             Onboard Project
           </Link>
           <Link
             href={'/forms/project-tower-edit'}
-            className='btn  flex items-center active:btn-success hover:text-black active:text-white'
+            className='btn flex items-center active:btn-success hover:text-black active:text-white'
           >
             Update Project
           </Link>
           <Link
             href={'/forms/image-tagging'}
-            className='btn  flex items-center active:btn-success hover:text-black active:text-white'
+            className='btn flex items-center active:btn-success hover:text-black active:text-white'
           >
             Image Tagging
           </Link>
