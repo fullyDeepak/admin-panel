8b8ed2947ff1d2b7181dbb7261a478a752183a31
Deepak Shreevastava <dpkshreevastava@gmail.com>
2024-08-02 16:34:05 +0530

    feat: add tw merge

--- src/components/ui/LoadingCircle.tsx
+++ src/components/ui/LoadingCircle.tsx
@@ -1,13 +1,21 @@
+import { cn } from '@/lib/utils';
+
 type LoadingCircleProps = {
   size: 'medium' | 'large';
-  circleColor: string;
+  circleColor?: string;
+  tailwindClass?: string;
 };
 
 export default function LoadingCircle({
   circleColor,
   size,
+  tailwindClass,
 }: LoadingCircleProps) {
   const width = size === 'large' ? 'loading-lg' : 'loading-md';
   const color = `bg-${circleColor}-500`;
-  return <div className={`loading loading-spinner ${width} ${color}`}></div>;
+  return (
+    <div
+      className={cn(`loading loading-spinner ${width} ${color}`, tailwindClass)}
+    ></div>
+  );
 }
