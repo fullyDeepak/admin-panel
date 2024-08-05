88b5c73adc84b228d1651ec1bcd05fd8ea2cf038
Deepak Shreevastava <dpkshreevastava@gmail.com>
2024-08-05 13:49:13 +0530

    feat: select all and in-place editor for keywords tag

---
+++ src/app/forms/project-tower-edit/EditableList.tsx
@@ -0,0 +1,98 @@
+import {
+  EditProjectTaggingType,
+  useEditProjectStore,
+} from '@/store/useEditProjectStore';
+import { nanoid } from 'nanoid';
+import { KeyboardEvent, useState } from 'react';
+import { RiCloseLine } from 'react-icons/ri';
+
+type AllowedKeys = Pick<
+  EditProjectTaggingType,
+  'developerKeywords' | 'landlordKeywords'
+>;
+
+type Props = {
+  keywordType: keyof AllowedKeys;
+};
+
+export default function EditableList({ keywordType }: Props) {
+  const { editProjectFormData, updateEditProjectFormData } =
+    useEditProjectStore();
+  const [inputValue, setInputValue] = useState('');
+  const [editingIndex, setEditingIndex] = useState<number | null>(null);
+
+  const handleDoubleClick = (index: number, value: string) => {
+    setEditingIndex(index);
+    setInputValue(value);
+  };
+  function handleDelete(item: string) {
+    const filteredKeywords = editProjectFormData[keywordType]?.filter(
+      (keyword) => keyword !== item
+    );
+    updateEditProjectFormData({
+      [keywordType]: filteredKeywords,
+    });
+  }
+
+  function handleOnBlur() {
+    if (editingIndex != null) {
+      const updatedList = [...editProjectFormData[keywordType]];
+      updatedList[editingIndex] = inputValue;
+      setEditingIndex(null);
+      updateEditProjectFormData({
+        [keywordType]: updatedList,
+      });
+    }
+  }
+
+  function handleKeyDown(e: KeyboardEvent<HTMLInputElement>) {
+    if (e.key === 'Enter' || e.key === 'Escape') {
+      handleOnBlur();
+    } else if (e.key === 'Tab' && editingIndex != null) {
+      e.preventDefault();
+      if (editProjectFormData[keywordType].length === editingIndex + 1) {
+        handleOnBlur();
+      } else {
+        setEditingIndex(editingIndex + 1);
+        setInputValue(editProjectFormData[keywordType][editingIndex + 1]);
+      }
+    }
+  }
+
+  return (
+    <ul className='flex w-full flex-col gap-4 overflow-y-auto rounded-box bg-green-100 py-4'>
+      {editProjectFormData[keywordType].map((item, index) => (
+        <li
+          className='mx-5 flex items-center justify-between border-b pb-2 text-sm'
+          key={nanoid()}
+          onDoubleClick={() => handleDoubleClick(index, item)}
+        >
+          {editingIndex === index ? (
+            <input
+              type='text'
+              className='w-[90%] rounded-md px-2 py-2 shadow outline-none'
+              value={inputValue}
+              onChange={(e) => {
+                setInputValue(e.target.value);
+              }}
+              onKeyDown={handleKeyDown}
+              onBlur={handleOnBlur}
+              autoFocus
+            />
+          ) : (
+            <a>{item}</a>
+          )}
+          <button
+            className='aspect-square rounded-full bg-red-200 p-[1px]'
+            type='button'
+            onClick={() => {
+              handleDelete(item);
+            }}
+          >
+            <RiCloseLine className='text-red-500' size={20} />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
--- src/app/forms/project-tower-edit/Keywords.tsx
+++ src/app/forms/project-tower-edit/Keywords.tsx
@@ -2,11 +2,11 @@ import Select from 'react-select';
 import { FiChevronRight } from 'react-icons/fi';
 import { useQuery } from '@tanstack/react-query';
 import axiosClient from '@/utils/AxiosClient';
-import { RiCloseLine } from 'react-icons/ri';
 import { useState } from 'react';
 import LoadingCircle from '@/components/ui/LoadingCircle';
 import { useEditProjectStore } from '@/store/useEditProjectStore';
 import { startCase } from 'lodash';
+import EditableList from './EditableList';
 
 export default function Keywords() {
   const { updateEditProjectFormData, editProjectFormData } =
@@ -54,8 +54,9 @@ export default function Keywords() {
     staleTime: Infinity,
     refetchOnWindowFocus: false,
   });
-  const [inputValue, setInputValue] = useState('');
-  const [selectedKeyword, setSelectedKeyword] = useState('');
+
+  const [selectedKeyword, setSelectedKeyword] = useState<string[]>([]);
+
   return (
     <>
       <label className='flex flex-wrap items-center justify-between gap-5'>
@@ -83,18 +84,47 @@ export default function Keywords() {
             <li className='menu-title text-center text-xl text-violet-600'>
               RERA Landlord Land Owner
             </li>
+            <button
+              className='btn btn-warning btn-sm max-w-fit self-center'
+              type='button'
+              onClick={() => {
+                let names: string[] = [];
+                keywordList.rera.map((projectKeywords) => {
+                  names = names.concat(projectKeywords.keyword_list);
+                });
+                if (editProjectFormData.keywordType?.value === 'landlord') {
+                  updateEditProjectFormData({
+                    landlordKeywords: [
+                      ...editProjectFormData.landlordKeywords,
+                      ...names,
+                    ],
+                  });
+                } else if (
+                  editProjectFormData.keywordType?.value === 'developer'
+                ) {
+                  updateEditProjectFormData({
+                    developerKeywords: [
+                      ...editProjectFormData.developerKeywords,
+                      ...names,
+                    ],
+                  });
+                }
+              }}
+            >
+              Select All
+            </button>
             <div className='overflow-y-auto'>
               {keywordList?.rera?.map((projectKeywords) =>
                 projectKeywords.keyword_list.map((keyword, index) => (
                   <label key={index} className='ml-3 flex flex-col'>
                     <input
-                      type='radio'
+                      type='checkbox'
                       className='peer'
                       hidden
                       name='rera-keyword-list'
                       onChange={(e) => {
                         if (e.target.checked) {
-                          setSelectedKeyword(keyword);
+                          setSelectedKeyword((prev) => [...prev]);
                         }
                       }}
                     />
@@ -106,10 +136,41 @@ export default function Keywords() {
               )}
             </div>
           </ul>
-          <ul className='menu mr-2 max-h-[90vh] w-full max-w-[300px] flex-nowrap overflow-y-auto rounded-box bg-violet-100 py-4'>
+          <ul className='menu mr-2 max-h-[90vh] w-full max-w-[400px] flex-nowrap overflow-y-auto rounded-box bg-violet-100 py-4'>
             <li className='menu-title text-center text-xl text-violet-600'>
               Transaction Land Owner
             </li>
+            <button
+              className='btn btn-warning btn-sm max-w-fit self-center'
+              type='button'
+              onClick={() => {
+                let names: string[] = [];
+                keywordList.transaction.map((transData) =>
+                  transData.count_names.map((partyName) => {
+                    names = names.concat(partyName.names);
+                  })
+                );
+                if (editProjectFormData.keywordType?.value === 'landlord') {
+                  updateEditProjectFormData({
+                    landlordKeywords: [
+                      ...editProjectFormData.landlordKeywords,
+                      ...names,
+                    ],
+                  });
+                } else if (
+                  editProjectFormData.keywordType?.value === 'developer'
+                ) {
+                  updateEditProjectFormData({
+                    developerKeywords: [
+                      ...editProjectFormData.developerKeywords,
+                      ...names,
+                    ],
+                  });
+                }
+              }}
+            >
+              Select All
+            </button>
             {keywordList?.transaction.map((transData, index) => (
               <li key={index}>
                 <details open={index === 0 ? true : false}>
@@ -117,18 +178,21 @@ export default function Keywords() {
                     {transData.simple_transaction_type}
                   </summary>
 
-                  <ul className='flex flex-col'>
-                    {transData.count_names.map((countNamesItem, i) =>
+                  <ul className='flex flex-col gap-y-1'>
+                    {transData.count_names.map((countNamesItem) =>
                       countNamesItem.names.map((partyName) => (
-                        <label key={i} className='ml-3 flex flex-col'>
+                        <label key={partyName} className='ml-3 flex flex-col'>
                           <input
-                            type='radio'
+                            type='checkbox'
                             className='group peer'
                             hidden
                             name='transaction-keyword-list'
                             onChange={(e) => {
                               if (e.target.checked) {
-                                setSelectedKeyword(partyName);
+                                setSelectedKeyword((prev) => [
+                                  partyName,
+                                  ...prev,
+                                ]);
                               }
                             }}
                           />
@@ -148,10 +212,9 @@ export default function Keywords() {
           </ul>
           <div className='flex flex-col items-center justify-center gap-5'>
             <button
-              className='btn'
+              className='btn btn-outline btn-sm'
               type='button'
               onClick={() => {
-                setInputValue(selectedKeyword);
                 const transCheckBoxes = document.getElementsByName(
                   'transaction-keyword-list'
                 ) as unknown as HTMLInputElement[];
@@ -164,10 +227,25 @@ export default function Keywords() {
                 reraCheckBoxes.forEach((item) => {
                   item.checked = false;
                 });
-                const editor = document.getElementById(
-                  'keyword-editor'
-                ) as HTMLInputElement;
-                editor.focus();
+                if (editProjectFormData.keywordType?.value === 'developer') {
+                  updateEditProjectFormData({
+                    developerKeywords: [
+                      ...selectedKeyword,
+                      ...editProjectFormData.developerKeywords,
+                    ],
+                  });
+                  setSelectedKeyword([]);
+                } else if (
+                  editProjectFormData.keywordType?.value === 'landlord'
+                ) {
+                  updateEditProjectFormData({
+                    landlordKeywords: [
+                      ...selectedKeyword,
+                      ...editProjectFormData.landlordKeywords,
+                    ],
+                  });
+                  setSelectedKeyword([]);
+                }
               }}
             >
               <FiChevronRight size={26} />
@@ -178,86 +256,12 @@ export default function Keywords() {
               Tagged Keywords:{' '}
               {startCase(editProjectFormData.keywordType.value)}
             </li>
-            <input
-              type='text'
-              className='mx-auto w-full max-w-[80%] rounded-md border-0 p-2 text-gray-900 shadow-sm outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-violet-600'
-              value={inputValue}
-              id='keyword-editor'
-              onChange={(e) => setInputValue(e.target.value)}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter' && inputValue.length > 0) {
-                  if (editProjectFormData.keywordType?.value === 'developer') {
-                    updateEditProjectFormData({
-                      developerKeywords: [
-                        inputValue,
-                        ...editProjectFormData.developerKeywords,
-                      ],
-                    });
-                    setInputValue('');
-                  } else if (
-                    editProjectFormData.keywordType?.value === 'landlord'
-                  ) {
-                    updateEditProjectFormData({
-                      landlordKeywords: [
-                        inputValue,
-                        ...editProjectFormData.landlordKeywords,
-                      ],
-                    });
-                    setInputValue('');
-                  }
-                }
-              }}
-            />
-            {editProjectFormData.keywordType?.value === 'developer' &&
-              editProjectFormData?.developerKeywords.map((item, index) => (
-                <li
-                  className='mx-5 flex items-center justify-between border-b pb-2 text-sm'
-                  key={index}
-                >
-                  <a>{item}</a>
-                  <button
-                    className='aspect-square rounded-full bg-red-200 p-[1px]'
-                    type='button'
-                    onClick={() => {
-                      const filteredDeveloperKeywords =
-                        editProjectFormData.developerKeywords.filter(
-                          (keyword) => keyword !== item
-                        );
-                      updateEditProjectFormData({
-                        developerKeywords: filteredDeveloperKeywords,
-                      });
-                    }}
-                  >
-                    <RiCloseLine className='text-red-500' size={20} />
-                  </button>
-                </li>
-              ))}
-            <ul className='flex w-full flex-col gap-4 overflow-y-auto rounded-box bg-green-100 py-4'>
-              {editProjectFormData.keywordType?.value === 'landlord' &&
-                editProjectFormData?.landlordKeywords.map((item, index) => (
-                  <li
-                    className='mx-5 flex items-center justify-between border-b pb-2 text-sm'
-                    key={index}
-                  >
-                    <a>{item}</a>
-                    <button
-                      className='aspect-square rounded-full bg-red-200 p-[1px]'
-                      type='button'
-                      onClick={() => {
-                        const filteredLandlordKeywords =
-                          editProjectFormData.landlordKeywords.filter(
-                            (keyword) => keyword !== item
-                          );
-                        updateEditProjectFormData({
-                          landlordKeywords: filteredLandlordKeywords,
-                        });
-                      }}
-                    >
-                      <RiCloseLine className='text-red-500' size={20} />
-                    </button>
-                  </li>
-                ))}
-            </ul>
+            {editProjectFormData.keywordType?.value === 'developer' && (
+              <EditableList keywordType='developerKeywords' />
+            )}
+            {editProjectFormData.keywordType?.value === 'landlord' && (
+              <EditableList keywordType='landlordKeywords' />
+            )}
           </ul>
         </div>
       )}
