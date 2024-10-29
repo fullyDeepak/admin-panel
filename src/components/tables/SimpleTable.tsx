df44ced57b22c7be0c42d14064845eeefe7e19c9
Deepak Shreevastava <dpkshreevastava@gmail.com>
2024-10-29 10:11:04 +0530

    chore: set default height

--- src/components/tables/SimpleTable.tsx
+++ src/components/tables/SimpleTable.tsx
@@ -5,7 +5,7 @@ type SimpleTableProps = {
 
 export default function SimpleTable({ columns, tableData }: SimpleTableProps) {
   return (
-    <div className='m-5 mt-0 max-h-[40vh] overflow-x-auto rounded-lg border border-gray-300 shadow-md'>
+    <div className='m-5 mt-0 max-h-[40vh] min-h-20 overflow-x-auto rounded-lg border border-gray-300 shadow-md'>
       <table className='relative w-full border-collapse bg-white text-sm text-gray-700'>
         <thead className='sticky top-0 z-[1] text-nowrap bg-gray-50 ring-1 ring-slate-200'>
           <tr>
