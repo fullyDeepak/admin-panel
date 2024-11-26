cdc3abb6dbbb138da78d72982f963c75483f9497
Deepak Shreevastava <dpkshreevastava@gmail.com>
2024-11-27 01:49:39 +0530

    feat: generic hook for data fetching

---
+++ src/app/hooks/useFetchData.ts
@@ -0,0 +1,19 @@
+import axiosClient from '@/utils/AxiosClient';
+import { useQuery } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
+
+const fetchData = async <T>(url: string): Promise<T> => {
+  const response: AxiosResponse<{ data: T }> = await axiosClient.get(url);
+
+  return response.data.data;
+};
+
+const useFetchData = <T>(url: string) => {
+  return useQuery<T, Error>({
+    queryKey: [url],
+    queryFn: () => fetchData<T>(url),
+    staleTime: 1000 * 60 * 5,
+  });
+};
+
+export default useFetchData;
