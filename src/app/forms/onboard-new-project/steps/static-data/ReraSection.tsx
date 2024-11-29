1e681f38605896a8770fe7b304817fed678f35ce
Deepak Shreevastava <dpkshreevastava@gmail.com>
2024-11-29 13:41:27 +0530

    fix: gfName

--- src/app/forms/onboard-new-project/steps/static-data/ReraSection.tsx
+++ src/app/forms/onboard-new-project/steps/static-data/ReraSection.tsx
@@ -170,6 +170,7 @@ export default function ReraSection({
         deleteFullUnitNos: '',
         exceptionUnitNos: '',
         towerDoorNoString: '',
+        gfName: '',
       };
     });
     console.log({ reraTotalLandAreaObj, reraCalcNetLandAreaObj });
