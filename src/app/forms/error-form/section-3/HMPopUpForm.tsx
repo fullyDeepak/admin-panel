21e12ebb9e77afcf1ebe13ffe71aa8e17a348398
Deepak Shreevastava <dpkshreevastava@gmail.com>
2024-12-02 00:57:13 +0530

    feat: live SQL build

--- src/app/forms/error-form/section-3/HMPopUpForm.tsx
+++ src/app/forms/error-form/section-3/HMPopUpForm.tsx
@@ -91,86 +91,104 @@ export default function HMPopUpForm() {
       <h3 className='pb-5 text-center text-xl font-semibold'>
         Popup Form: HM Search + Fill
       </h3>
-      <div className='flex max-w-sm flex-col gap-2'>
-        <div className='flex items-center gap-5'>
-          <span className='flex-[2]'>Locality:</span>
-          <input
-            className={cn(inputBoxClass, 'ml-0 flex-[6]')}
-            placeholder='Enter Locality'
-            type='text'
-            value={formState['locality']}
-            onChange={(e) =>
-              setFormState((prev) => ({
-                ...prev,
-                locality: e.target.value,
-              }))
-            }
-          />
+      <div className='grid grid-cols-3 gap-5'>
+        <div className='col-span-2 flex w-full justify-evenly'>
+          <div className='flex max-w-sm flex-col gap-2'>
+            <div className='flex items-center gap-5'>
+              <span className='flex-[2]'>Locality:</span>
+              <input
+                className={cn(inputBoxClass, 'ml-0 flex-[6]')}
+                placeholder='Enter Locality'
+                type='text'
+                value={formState['locality']}
+                onChange={(e) =>
+                  setFormState((prev) => ({
+                    ...prev,
+                    locality: e.target.value,
+                  }))
+                }
+              />
+            </div>
+            <div className='flex items-center gap-5'>
+              <span className='flex-[2]'>Door No:</span>
+              <input
+                className={inputBoxClass}
+                placeholder='Enter Door No'
+                type='text'
+                value={formState['doorNo']}
+                onChange={(e) =>
+                  setFormState((prev) => ({
+                    ...prev,
+                    doorNo: e.target.value,
+                  }))
+                }
+              />
+              <label className={cn('swap swap-flip rounded p-1 font-semibold')}>
+                <input
+                  type='checkbox'
+                  checked={formState['doorNoFlag']}
+                  onChange={(e) => {
+                    setFormState((prev) => ({
+                      ...prev,
+                      doorNoFlag: e.target.checked,
+                    }));
+                  }}
+                />
+                <div className='swap-on'>AND</div>
+                <div className='swap-off'>&nbsp;OR</div>
+              </label>
+            </div>
+            <div className='flex items-center gap-5'>
+              <span className='flex-[2]'>Owner Name:</span>
+              <input
+                className={inputBoxClass}
+                placeholder='Enter Owner Name'
+                type='text'
+                value={formState['ownerName']}
+                onChange={(e) =>
+                  setFormState((prev) => ({
+                    ...prev,
+                    ownerName: e.target.value,
+                  }))
+                }
+              />
+              <label className={cn('swap swap-flip rounded p-1 font-semibold')}>
+                <input
+                  type='checkbox'
+                  checked={formState['ownerNameFlag']}
+                  onChange={(e) => {
+                    setFormState((prev) => ({
+                      ...prev,
+                      ownerNameFlag: e.target.checked,
+                    }));
+                  }}
+                />
+                <div className='swap-on'>AND</div>
+                <div className='swap-off'>&nbsp;OR</div>
+              </label>
+            </div>
+            <button
+              className='btn btn-neutral btn-sm max-w-fit self-center'
+              onClick={handleSearch}
+            >
+              Search HM
+            </button>
+          </div>
         </div>
-        <div className='flex items-center gap-5'>
-          <span className='flex-[2]'>Door No:</span>
-          <input
-            className={inputBoxClass}
-            placeholder='Enter Door No'
-            type='text'
-            value={formState['doorNo']}
-            onChange={(e) =>
-              setFormState((prev) => ({
-                ...prev,
-                doorNo: e.target.value,
-              }))
-            }
-          />
-          <label className={cn('swap swap-flip rounded p-1 font-semibold')}>
-            <input
-              type='checkbox'
-              checked={formState['doorNoFlag']}
-              onChange={(e) => {
-                setFormState((prev) => ({
-                  ...prev,
-                  doorNoFlag: e.target.checked,
-                }));
-              }}
-            />
-            <div className='swap-on'>AND</div>
-            <div className='swap-off'>&nbsp;OR</div>
-          </label>
+        <div>
+          <span>SQL Query:</span>
+          <pre className='max-h-[500px] overflow-y-auto bg-gray-100 p-3 font-mono text-sm outline outline-1 outline-gray-300'>
+            {`SELECT * 
+FROM 
+    public.house_master
+WHERE 
+    "source" IS NOT NULL
+    ${formState.locality ? `AND locality ILIKE '${'%' + formState.locality + '%'}'` : ``}
+    ${formState.doorNo ? (formState.doorNoFlag ? `AND house_no ILIKE '${'%' + formState.doorNo + '%'}'` : `OR house_no ILIKE '${'%' + formState.doorNo + '%'}'`) : ``}
+    ${formState.ownerName ? (formState.ownerNameFlag ? `AND current_owner ILIKE '${'%' + formState.ownerName + '%'}'` : `OR current_owner ILIKE '${'%' + formState.ownerName + '%'}'`) : ``}
+    LIMIT 200;`}
+          </pre>
         </div>
-        <div className='flex items-center gap-5'>
-          <span className='flex-[2]'>Owner Name:</span>
-          <input
-            className={inputBoxClass}
-            placeholder='Enter Owner Name'
-            type='text'
-            value={formState['ownerName']}
-            onChange={(e) =>
-              setFormState((prev) => ({
-                ...prev,
-                ownerName: e.target.value,
-              }))
-            }
-          />
-          <label className={cn('swap swap-flip rounded p-1 font-semibold')}>
-            <input
-              type='checkbox'
-              checked={formState['ownerNameFlag']}
-              onChange={(e) => {
-                setFormState((prev) => ({
-                  ...prev,
-                  ownerNameFlag: e.target.checked,
-                }));
-              }}
-            />
-            <div className='swap-on'>AND</div>
-            <div className='swap-off'>&nbsp;OR</div>
-          </label>
-        </div>
-        <button
-          className='btn btn-neutral btn-sm max-w-fit self-center'
-          onClick={handleSearch}
-        >
-          Search HM
-        </button>
       </div>
       <div className='mt-5 max-w-full overflow-x-auto rounded-lg border border-gray-200 shadow-md'>
         {results && results.length > 0 && (
--- src/app/forms/error-form/section-3/TMPopUpForm.tsx
+++ src/app/forms/error-form/section-3/TMPopUpForm.tsx
@@ -18,7 +18,7 @@ export default function TMPopUpForm({ docId }: Props) {
     linkedDocFlag: true,
     counterPartyFlag: true,
     village: '',
-    projectId: '',
+    projectId: 0,
     counterParty: '',
     linkedDoc: '',
   });
@@ -117,144 +117,165 @@ export default function TMPopUpForm({ docId }: Props) {
       <h3 className='pb-5 text-center text-xl font-semibold'>
         Popup Form: TM Search + Fill
       </h3>
-      <div className='flex max-w-md flex-col gap-2'>
-        <div className='flex items-center gap-5'>
-          <span className='flex-[2]'>Doc Id:</span>
-          <input
-            className={cn(inputBoxClass, 'ml-0 flex-[6]')}
-            placeholder='Enter Doc Id'
-            type='text'
-            value={formState.docId}
-            onChange={(e) =>
-              setFormState((prev) => ({
-                ...prev,
-                docId: e.target.value,
-              }))
-            }
-          />
+      <div className='grid grid-cols-3 gap-5'>
+        <div className='col-span-2 flex w-full justify-evenly'>
+          <div className='flex max-w-md flex-col gap-2'>
+            <div className='flex items-center gap-5'>
+              <span className='flex-[2]'>Doc Id:</span>
+              <input
+                className={cn(inputBoxClass, 'ml-0 flex-[6]')}
+                placeholder='Enter Doc Id'
+                type='text'
+                value={formState.docId}
+                onChange={(e) =>
+                  setFormState((prev) => ({
+                    ...prev,
+                    docId: e.target.value,
+                  }))
+                }
+              />
+            </div>
+            <div className='flex items-center gap-5'>
+              <span className='flex-[2]'>Village:</span>
+              <input
+                className={inputBoxClass}
+                placeholder='Enter Village'
+                type='text'
+                value={formState.village}
+                onChange={(e) =>
+                  setFormState((prev) => ({
+                    ...prev,
+                    village: e.target.value,
+                  }))
+                }
+              />
+              <label className={cn('swap swap-flip rounded p-1 font-semibold')}>
+                <input
+                  type='checkbox'
+                  checked={formState.villageFlag}
+                  onChange={(e) => {
+                    setFormState((prev) => ({
+                      ...prev,
+                      villageFlag: e.target.checked,
+                    }));
+                  }}
+                />
+                <div className='swap-on'>AND</div>
+                <div className='swap-off'>&nbsp;OR</div>
+              </label>
+            </div>
+            <div className='flex items-center gap-5'>
+              <span className='flex-[2]'>Project Id:</span>
+              <input
+                className={inputBoxClass}
+                placeholder='Enter Project Id'
+                type='number'
+                value={formState.projectId || ''}
+                onChange={(e) =>
+                  setFormState((prev) => ({
+                    ...prev,
+                    projectId: e.target.valueAsNumber,
+                  }))
+                }
+              />
+              <label className={cn('swap swap-flip rounded p-1 font-semibold')}>
+                <input
+                  type='checkbox'
+                  checked={formState.projectIdFlag}
+                  onChange={(e) => {
+                    setFormState((prev) => ({
+                      ...prev,
+                      projectIdFlag: e.target.checked,
+                    }));
+                  }}
+                />
+                <div className='swap-on'>AND</div>
+                <div className='swap-off'>&nbsp;OR</div>
+              </label>
+            </div>
+            <div className='flex items-center gap-5'>
+              <span className='flex-[2]'>Counter Party:</span>
+              <input
+                className={inputBoxClass}
+                placeholder='Enter Counter Party'
+                type='text'
+                value={formState.counterParty}
+                onChange={(e) =>
+                  setFormState((prev) => ({
+                    ...prev,
+                    counterParty: e.target.value,
+                  }))
+                }
+              />
+              <label className={cn('swap swap-flip rounded p-1 font-semibold')}>
+                <input
+                  type='checkbox'
+                  checked={formState.counterPartyFlag}
+                  onChange={(e) => {
+                    setFormState((prev) => ({
+                      ...prev,
+                      counterPartyFlag: e.target.checked,
+                    }));
+                  }}
+                />
+                <div className='swap-on'>AND</div>
+                <div className='swap-off'>&nbsp;OR</div>
+              </label>
+            </div>
+            <div className='flex items-center gap-5'>
+              <span className='flex-[2]'>Linked Doc:</span>
+              <input
+                className={inputBoxClass}
+                placeholder='Enter Linked Doc'
+                type='text'
+                value={formState.linkedDoc}
+                onChange={(e) =>
+                  setFormState((prev) => ({
+                    ...prev,
+                    linkedDoc: e.target.value,
+                  }))
+                }
+              />
+              <label className={cn('swap swap-flip rounded p-1 font-semibold')}>
+                <input
+                  type='checkbox'
+                  checked={formState.linkedDocFlag}
+                  onChange={(e) => {
+                    setFormState((prev) => ({
+                      ...prev,
+                      linkedDocFlag: e.target.checked,
+                    }));
+                  }}
+                />
+                <div className='swap-on'>AND</div>
+                <div className='swap-off'>&nbsp;OR</div>
+              </label>
+            </div>
+            <button
+              className='btn btn-neutral btn-sm max-w-fit self-center'
+              onClick={handleSearch}
+            >
+              Search TM
+            </button>
+          </div>
         </div>
-        <div className='flex items-center gap-5'>
-          <span className='flex-[2]'>Village:</span>
-          <input
-            className={inputBoxClass}
-            placeholder='Enter Village'
-            type='text'
-            value={formState.village}
-            onChange={(e) =>
-              setFormState((prev) => ({
-                ...prev,
-                village: e.target.value,
-              }))
-            }
-          />
-          <label className={cn('swap swap-flip rounded p-1 font-semibold')}>
-            <input
-              type='checkbox'
-              checked={formState.villageFlag}
-              onChange={(e) => {
-                setFormState((prev) => ({
-                  ...prev,
-                  villageFlag: e.target.checked,
-                }));
-              }}
-            />
-            <div className='swap-on'>AND</div>
-            <div className='swap-off'>&nbsp;OR</div>
-          </label>
+        <div>
+          <span>SQL Query:</span>
+          <pre className='max-h-[500px] overflow-y-auto bg-gray-100 p-3 font-mono text-sm outline outline-1 outline-gray-300'>
+            {`SELECT 
+    * 
+FROM 
+    transactions.records
+WHERE 
+    doc_id IS NOT NULL
+    ${`AND doc_id = ${formState.docId}`}
+    ${formState.village ? (formState.villageFlag ? `AND village = ${formState.village}` : `OR village = ${formState.village}`) : ``}
+    ${formState.projectId ? (formState.projectIdFlag ? `AND project_id = ${formState.projectId}` : `OR project_id = ${formState.projectId}`) : ''}
+    ${formState.counterParty ? (formState.counterPartyFlag ? `AND party_details ILIKE ${'%' + formState.counterParty + '%'}` : `OR party_details ILIKE${'%' + formState.counterParty + '%'}`) : ``}
+    ${formState.linkedDoc ? (formState.linkedDocFlag ? `AND linked_docs ILIKE ${'%' + formState.linkedDoc + '%'}` : `OR linked_doc ILIKE ${'%' + formState.linkedDoc + '%'}`) : ``}
+    LIMIT 200;`}
+          </pre>
         </div>
-        <div className='flex items-center gap-5'>
-          <span className='flex-[2]'>Project Id:</span>
-          <input
-            className={inputBoxClass}
-            placeholder='Enter Project Id'
-            type='text'
-            value={formState.projectId}
-            onChange={(e) =>
-              setFormState((prev) => ({
-                ...prev,
-                projectId: e.target.value,
-              }))
-            }
-          />
-          <label className={cn('swap swap-flip rounded p-1 font-semibold')}>
-            <input
-              type='checkbox'
-              checked={formState.projectIdFlag}
-              onChange={(e) => {
-                setFormState((prev) => ({
-                  ...prev,
-                  projectIdFlag: e.target.checked,
-                }));
-              }}
-            />
-            <div className='swap-on'>AND</div>
-            <div className='swap-off'>&nbsp;OR</div>
-          </label>
-        </div>
-        <div className='flex items-center gap-5'>
-          <span className='flex-[2]'>Counter Party:</span>
-          <input
-            className={inputBoxClass}
-            placeholder='Enter Counter Party'
-            type='text'
-            value={formState.counterParty}
-            onChange={(e) =>
-              setFormState((prev) => ({
-                ...prev,
-                counterParty: e.target.value,
-              }))
-            }
-          />
-          <label className={cn('swap swap-flip rounded p-1 font-semibold')}>
-            <input
-              type='checkbox'
-              checked={formState.counterPartyFlag}
-              onChange={(e) => {
-                setFormState((prev) => ({
-                  ...prev,
-                  counterPartyFlag: e.target.checked,
-                }));
-              }}
-            />
-            <div className='swap-on'>AND</div>
-            <div className='swap-off'>&nbsp;OR</div>
-          </label>
-        </div>
-        <div className='flex items-center gap-5'>
-          <span className='flex-[2]'>Linked Doc:</span>
-          <input
-            className={inputBoxClass}
-            placeholder='Enter Linked Doc'
-            type='text'
-            value={formState.linkedDoc}
-            onChange={(e) =>
-              setFormState((prev) => ({
-                ...prev,
-                linkedDoc: e.target.value,
-              }))
-            }
-          />
-          <label className={cn('swap swap-flip rounded p-1 font-semibold')}>
-            <input
-              type='checkbox'
-              checked={formState.linkedDocFlag}
-              onChange={(e) => {
-                setFormState((prev) => ({
-                  ...prev,
-                  linkedDocFlag: e.target.checked,
-                }));
-              }}
-            />
-            <div className='swap-on'>AND</div>
-            <div className='swap-off'>&nbsp;OR</div>
-          </label>
-        </div>
-        <button
-          className='btn btn-neutral btn-sm max-w-fit self-center'
-          onClick={handleSearch}
-        >
-          Search TM
-        </button>
       </div>
       <div className='mt-5 max-w-full overflow-x-auto rounded-lg border border-gray-200 shadow-md'>
         {results && results.length > 0 && (
