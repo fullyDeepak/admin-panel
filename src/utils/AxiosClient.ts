05b6f55fbadc8997b4af8b30e4168990b6a58b32
Deepak Shreevastava <dpkshreevastava@gmail.com>
2024-12-05 18:01:29 +0530

    chore: new dps for data fetching

--- src/utils/AxiosClient.ts
+++ src/utils/AxiosClient.ts
@@ -1,6 +1,11 @@
 import axios from 'axios';
+import xior from 'xior';
 
 const axiosClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_API,
+});
+
+export const http = xior.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_API,
 });
 
